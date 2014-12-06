a0f954b886e814920e3623a9a2f57952f48e6d26
xnieamo <xiaomao_ding@yahoo.com>
2014-12-06 11:56:03 -0500

    Added mail

--- FirstDraft/src/java/MatchServlet.java
+++ FirstDraft/src/java/MatchServlet.java
@@ -12,8 +12,16 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.security.InvalidKeyException;
 import java.time.Clock;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -46,5 +54,40 @@ public class MatchServlet extends HttpServlet {
             res.sendRedirect(res.encodeRedirectURL("index.html"));
 
     }
-
+    public void email(){
+        final String username = "letsexchangegoods@gmail.com";
+		final String password = "bananapan";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+                        @Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("letsexchangegoods@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("8562468429@tmomail.net"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+    }
 }
