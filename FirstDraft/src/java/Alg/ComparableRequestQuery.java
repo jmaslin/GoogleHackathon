51f13acf8f3a5ca61c336529ebae12c799653f2b
xnieamo <xiaomao_ding@yahoo.com>
2014-12-06 18:07:52 -0500

    a

---
+++ FirstDraft/src/java/Alg/ComparableRequestQuery.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package Alg;
+
+import java.awt.geom.Point2D;
+
+
+/**
+ * Query with comparable functionality to allow for a localization of services  
+ * 
+ * @author Cam
+ */
+
+
+public class ComparableRequestQuery implements Comparable<Object> {
+
+	private String name;
+	private String t;
+	private String keyword; //possibly enum for simplification
+	private Point2D.Double coord;
+	private double distance; //units
+	
+
+	
+	public ComparableRequestQuery(String t, String keyword,Point2D.Double coord, String name){
+		this.t = t;
+		this.keyword = keyword;
+		this.coord = coord;
+		this.name = name;
+	}
+	
+	/**
+	 * @return Returns the location of a query as a String
+	 */
+	public Point2D.Double getLocation(){
+		return coord;
+	}
+	
+	/**
+	 * @return Returns the type of the query as a Type enum
+	 */
+	public String getType(){
+		return t;
+	}
+	
+	/**
+	 * Method to set the distance to a location
+	 * 
+	 * @param distance The new distance
+	 */
+	public void setDistance(double dist){
+		distance = dist; 
+	}
+	
+	/**
+	 * @return Returns the distance to the location in question
+	 */
+	public double getDistance(){
+		return distance;
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
+	
+	
+	/**
+	 * Method to compare queries based on distance to request/service
+	 * 
+	 * @param other The query to which this one will be compared
+	 */
+	@Override
+	public int compareTo(Object other) {
+		double diff = distance - ((ComparableRequestQuery) other).getDistance();
+		if(diff < 0) return -1;
+		else if (diff > 0) return 1;
+		else return 0;
+	}
+
+}
