599170c2ae4f982915c1c1a766c4f1675bb6de61
djs317-cse2-f14 <djs317@lehigh.edu>
2014-12-07 00:34:54 -0500

    Oops forgot the actual file

---
+++ FirstDraft/src/java/MarketPlace.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Alg.ComparableRequestQuery;
+import java.awt.geom.Point2D;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.security.InvalidKeyException;
+import java.time.Clock;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Xiaomao
+ */
+public class MarketPlace extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+
+
+    }
+
+}
