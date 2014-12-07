f235cda5a2201fccf354a9e5253966aa955040c5
xnieamo <xiaomao_ding@yahoo.com>
2014-12-06 19:03:52 -0500

    Revert "WORKS"
    This reverts commit d63478edf0af50179aa2ce58736dcf2a72754098.

--- FirstDraft/src/java/MatchServlet.java
+++ FirstDraft/src/java/MatchServlet.java
@@ -39,39 +39,35 @@ public class MatchServlet extends HttpServlet {
             throws IOException, ServletException {
         //System.out.println("Hello");
          //   res.setContentType("application/json;charset=UTF-8");
-            //String id = req.getParameter("rowID");
+            String id = req.getParameter("rowID");
             
             String action = req.getParameter("purposeRadio");
             String item = req.getParameter("requestType");
             String locLat = req.getParameter("locLat"); 
             String locLng = req.getParameter("locLng"); 
-            String phone = req.getParameter("contactPhone");
-            String email = req.getParameter("contactEmail");
+            String contact = req.getParameter("contactPhone");
             String loc = req.getParameter("locLng");
             String lat = req.getParameter("locLat");
             
         try {
-           ComparableRequestQuery t = Alg.AlgUtil.ALG.newQuery(action, item, new Point2D.Double(0, 0), phone);
+           ComparableRequestQuery t = Alg.AlgUtil.ALG.newQuery(action, item, new Point2D.Double(0, 0), contact);
           
            if (t != null){
                //sent stuff
-                System.out.println("MATCH!");
-               email(phone, email, "hello", "world");
-              
+               //email();
+               System.out.println("MATCH!");
            }
         } catch (InvalidKeyException ex) {
             Logger.getLogger(MatchServlet.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("My bad");
         }
-          //  System.out.println(loc);
-         System.out.println("email:"+email);
-          System.out.println("phone:"+phone);
-            System.out.println(Alg.AlgUtil.ALG.list.get(0).values().size());
-           // res.sendRedirect(res.encodeRedirectURL("index.html"));
+            System.out.println(loc);
+            System.out.println(Alg.AlgUtil.ALG.list.get(0).size());
+            res.sendRedirect(res.encodeRedirectURL("index.html"));
 
     }
     public void email(String number, String email, String messageText, String keyword){
-                final String username = "letsexchangegoods@gmail.com";
+        final String username = "letsexchangegoods@gmail.com";
 		final String password = "bananapan";
  
 		Properties props = new Properties();
@@ -79,7 +75,7 @@ public class MatchServlet extends HttpServlet {
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
-                //   System.out.print("here");
+ 
 		Session session = Session.getInstance(props,
 		  new javax.mail.Authenticator() {
                         @Override
@@ -91,17 +87,15 @@ public class MatchServlet extends HttpServlet {
 		try {
  
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("me@gmail.com"));
+			message.setFrom(new InternetAddress("letsexchangegoods@gmail.com"));
                         if (number != null){
-                            
                             message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(number+"@tmomail.net"));
                             message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(number+"@txt.att.net"));
                             message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(number+"@vtext.com"));
                             message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(number+"@messaging.sprintpcs.com"));
                         }
                         if (email != null)
-                            if(!email.equals(""))
-                                message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+                            message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
  
                         message.setSubject("Match found: "+keyword);
 			message.setText(messageText);
