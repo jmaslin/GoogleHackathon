af9ebf65f0a1ef7479045f1bcb4ab77e4c874cd4
xnieamo <xiaomao_ding@yahoo.com>
2014-12-07 07:59:53 -0500

    q

---
+++ FirstDraft/build/web/convo/text.php
@@ -0,0 +1,88 @@
+<?php
+
+if ($_GET['type'] == "convos") {
+	$groups = array(
+		array(
+			"name" => "lolsci", 
+			"creator" => "name",
+			"type" => "convo",
+			"members" => "Bob",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "trade", 
+			"creator" => "lols",
+			"type" => "convo",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "store", 
+			"creator" => "name",
+			"type" => "convo",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "mcdonalds", 
+			"creator" => "lols",
+			"type" => "convo",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "police-watch", 
+			"creator" => "name",
+			"type" => "convo",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "government-news", 
+			"creator" => "lols",
+			"type" => "convo",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "commuity", 
+			"creator" => "lols",
+			"type" => "convo",
+			"tags" => array("one", "two", "three")
+		)
+	);
+}
+
+if ($_GET['type'] == "shops") {
+	$groups = array(
+		array(
+			"name" => "SHOP1", 
+			"creator" => "name",
+			"type" => "shops",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "SHOP12", 
+			"creator" => "lols",
+			"type" => "shops",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "SHOP134", 
+			"creator" => "name",
+			"type" => "shops",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "mcdonalds", 
+			"creator" => "lols",
+			"type" => "shops",
+			"tags" => array("one", "two", "three")
+		),
+		array(
+			"name" => "lolshops", 
+			"creator" => "name",
+			"type" => "shops",
+			"tags" => array("one", "two", "three")
+		)
+	);	
+}
+
+	echo json_encode($groups);
+
+?>
